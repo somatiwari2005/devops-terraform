77610d9feaa346462e23825aff1e49c375962d97
chandra <scsr.chandu7@gmail.com>
2019-12-16 18:59:41 +0530

    multiple ips

--- micro.tf
+++ micro.tf
@@ -3,14 +3,15 @@ access_key = "${var.access_key}"
 secret_key = "${var.secret_key}"
 region = "sa-east-1"
 }
-resource "aws_instance" "terraform" {
+resource "aws_instance" "terraform"{
+count = '2'
 ami = "ami-00023c695a5493524"
 instance_type = "t2.micro"
 key_name = "928f9f6274114cbfb31e59f2139de568-key"
 }
-variable "instance_count" {
-  default = "2"
+tags = {
+    Name  = "Terraform-${count.index + 1}"
 }
-output "ips" {
-  value = ["${aws_instance.terraform.*.public_ip}"]
+output "ip" {
+    value = "${join(",", aws_instance.web.*.public_ip)}"
 }
