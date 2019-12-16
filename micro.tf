9fd9cf4cb825ca8e7fd7bb935cc72fbe9f1d18df
chandra <scsr.chandu7@gmail.com>
2019-12-16 19:47:39 +0530

    removed tags

--- micro.tf
+++ micro.tf
@@ -9,9 +9,6 @@ ami = "ami-00023c695a5493524"
 instance_type = "t2.micro"
 key_name = "928f9f6274114cbfb31e59f2139de568-key"
 }
-tags = {
-    Name  = "Terraform-${count.index + 1}"
-}
 output "ip" {
     value = "${join(",", aws_instance.web.*.public_ip)}"
 }
