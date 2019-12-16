537b8c2811c5b808c9ab77b77f2fc1b80c0bc776
chandra <scsr.chandu7@gmail.com>
2019-12-16 18:06:38 +0530

    to get ip output

--- micro.tf
+++ micro.tf
@@ -1,9 +1,16 @@
 provider "aws" {
 access_key = "${var.access_key}"
 secret_key = "${var.secret_key}"
-region = "us-east-2"
+region = "sa-east-1"
 }
 resource "aws_instance" "terraform" {
-ami = "ami-0d03add87774b12c5"
+ami = "ami-00138b07206d4ceaf"
 instance_type = "t2.micro"
+key_name = "928f9f6274114cbfb31e59f2139de568-key"
+}
+variable "instance_count" {
+  default = "2"
+}
+output "ips" {
+  value = ["${aws_instance.terraform.*.public_ip}"]
 }
