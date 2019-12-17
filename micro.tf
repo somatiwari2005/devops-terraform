e0b3c25327498125e01002f723ab06ba1b0ad55f
chandra <scsr.chandu7@gmail.com>
2019-12-17 12:53:09 +0530

    change ip

--- micro.tf
+++ micro.tf
@@ -5,7 +5,7 @@ region = "sa-east-1"
 }
 resource "aws_instance" "terraform"{
 count = "2"
-ami = "ami-00023c695a5493524"
+ami = "ami-00145d9bdd54f0bc2"
 instance_type = "t2.micro"
 key_name = "928f9f6274114cbfb31e59f2139de568-key"
 }
