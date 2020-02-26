35043632bb4ff85a4abb92672640848a1e3ef73c
chandu412 <48725425+chandu412@users.noreply.github.com>
2020-02-26 15:03:03 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,4 +1,8 @@
 variable "access_key" {}
 variable "secret_key" {}
-variable "ami" {}
-variable "ssh_key" {}
+variable "ami" {
+  default = "ami-00145d9bdd54f0bc2"
+  }
+variable "ssh_key" {
+  default = "928f9f6274114cbfb31e59f2139de568-key"
+  }
