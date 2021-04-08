484b6127c4b4d737ec0f281854ad4efadb24854b
somatiwari2005 <somatiwari2005@yahoo.co.in>
2021-04-08 11:03:19 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -6,3 +6,11 @@ variable "ami" {
 variable "ssh_key" {
   default = "928f9f6274114cbfb31e59f2139de568-key"
   }
+
+variable "network_address_space" {                               
+     default = "10.1.0.0/16"                                                                                                                                                   default = "10.1.0.0/16"
+}
+
+variable "subnet_address_space" {                               
+     default = "10.1.0.0/24"                                                                                                                                                   default = "10.1.0.0/16"
+}
