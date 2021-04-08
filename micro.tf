7fa2cbfed0092237898db368de8978fecf12fd32
somatiwari2005 <somatiwari2005@yahoo.co.in>
2021-04-08 10:31:33 +0530

    Update micro.tf

--- micro.tf
+++ micro.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-access_key = "${var.access_key}"
-secret_key = "${var.secret_key}"
+access_key = var.access_key
+secret_key = var.secret_key
 region = "sa-east-1"
 }
 resource "aws_instance" "terraform"{
 count = "2"
-ami = "${var.ami}"
+ami = var.ami
 instance_type = "t2.micro"
-key_name = "${var.ssh_key}"
+key_name = var.ssh_key
 }
 output "ip" {
     value = "${join(",", aws_instance.terraform.*.public_ip)}"
